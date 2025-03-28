e1f011c3ab1750c18c75994c405ccbcc6b810019
Jiarong Tang <57526773+jiarongTang@users.noreply.github.com>
2025-03-28 10:54:27 +0100

    cambio

--- main.js
+++ main.js
@@ -184,22 +184,6 @@ const products = [
         price: 49.99,
         type: 'Videojuego',
         image: './assets/pokemonEspada.webp'
-      },
-      {
-        name: 'HP 255 G8 AMD',
-        price: 289,
-        stars: 4,
-        reviews: 250,
-        type: 'Libro',
-        image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
-      },
-      {
-        name: 'HP 255 G8 AMD',
-        price: 289,
-        stars: 4,
-        reviews: 250,
-        type: 'Libro',
-        image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
       }
     // Añade aquí al menos 9 productos más para tener un total de 10 productos
     // puedes cambiar los campos de cada objeto si es necesario para tu diseño...
